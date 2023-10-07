a229af9e86c9a4e558db4c023aa748d4fa9f86f5
BocchiTheProg <bocchitheprog@gmail.com>
2023-10-07 20:02:23 +0300

    Added additional elements in hash, fixed the problem about converting from decimal to Roman (4, 9, etc.).

--- Main.rb
+++ Main.rb
@@ -1,11 +1,11 @@
 require './RomanDecimal'
 
-input = "CIC"
+input = "MCDXLIV"
 puts "Convert Roman to Decimal"
 puts "Input from code:#{input} -> #{RomanDecimal.to_decimal(input)}"
 puts "Input from console:#{ARGV[0]} -> #{RomanDecimal.to_decimal(ARGV[0])}" unless ARGV.empty?
 
-input_int = 4
+input_int = 94
 puts "Convert Decimal to Roman"
 puts "Input from code:#{input_int} -> #{RomanDecimal.to_roman(input_int)}"
 puts "Input from console:#{ARGV[1]} -> #{RomanDecimal.to_roman(ARGV[1].to_i)}" unless ARGV[1].nil?
