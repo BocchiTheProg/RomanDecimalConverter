3b4fd7c04c353f97dec7e99b4f822fafee612f3b
BocchiTheProg <bocchitheprog@gmail.com>
2023-10-08 14:27:55 +0300

    Only take input from console, can input both decimal or Roman and convert to one another.

--- Main.rb
+++ Main.rb
@@ -1,11 +1,23 @@
 require './RomanDecimal'
 
-input = "MCDXLIV"
-puts "Convert Roman to Decimal"
-puts "Input from code:#{input} -> #{RomanDecimal.to_decimal(input)}"
-puts "Input from console:#{ARGV[0]} -> #{RomanDecimal.to_decimal(ARGV[0])}" unless ARGV.empty?
-
-input_int = 944
-puts "Convert Decimal to Roman"
-puts "Input from code:#{input_int} -> #{RomanDecimal.to_roman(input_int)}"
-puts "Input from console:#{ARGV[1]} -> #{RomanDecimal.to_roman(ARGV[1].to_i)}" unless ARGV[1].nil?
+if ARGV.empty?
+  puts "Sorry, the input is empty"
+elsif ARGV[0].match? /^\d+$/ # using regexes to validate input (only digits)
+  decimal = ARGV[0].to_i
+  if decimal < 1 or decimal >= 4000
+    puts "Input is out of range (1 <= input < 4000)"
+  else
+    puts "Input from console:#{ARGV[0]} -> #{RomanDecimal.to_roman(decimal)}"
+  end
+elsif ARGV[0].upcase.match? /^[IVXLCDM]+$/ # using regexes to validate input (only Roman numerals
+  roman = ARGV[0].upcase
+  res = RomanDecimal.to_decimal(roman)
+  if res < 1 or res >= 4000
+    puts "Input is out of range (1 <= input < 4000)"
+  else
+    puts "Input from console:#{ARGV[0]} -> #{res}"
+  end
+else
+  puts "Input include different systems or impossible letters"
+end
+puts "You can input Roman numeric and it will be converted to decimal number or vice versa"
--- RomanDecimal.rb
+++ RomanDecimal.rb
@@ -1,4 +1,5 @@
 module RomanDecimal
+  # Hash with Roman numerals (keys) and equivalent decimal numbers
   ROMAN_ARABIC = {"I" => 1,
                   "IV" => 4,
                   "V" => 5,
@@ -15,6 +16,7 @@ module RomanDecimal
 
   def self.to_decimal(roman)
     res = 0
+    # getting chars of string and indexes to have access to next char from current
     roman.each_char.with_index do |char, i|
       if i < roman.size - 1 and ROMAN_ARABIC[char] < ROMAN_ARABIC[roman[i+1]]
         res -= ROMAN_ARABIC[char]
@@ -27,6 +29,7 @@ module RomanDecimal
 
   def self.to_roman(decimal)
     res = ""
+    # getting array of keys and going through it in reverse order (from higher numbers to lowest)
     ROMAN_ARABIC.keys.reverse_each do |key|
       loop do
         break if decimal < ROMAN_ARABIC[key]
